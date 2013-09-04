b879d61c7cb3d7eec6c7867693f19d23f1d7fbd4
Aaron Boodman <aa@chromium.org>
2013-09-03 20:56:11 -0700

    I has a map.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Enable the newer UI look
+google.maps.visualRefresh = true;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                mapOptions);
+}
+
+window.addEventListener('load', initialize);
